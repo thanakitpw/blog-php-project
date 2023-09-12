4af1354f95e42b94572c0daa6f9fa20ddd0ef5ae
BankThanakit <thanakitpw@gmail.com>
2023-09-12 14:41:34 +0700

    add blog-detail.html

--- asset/js/main.js
+++ asset/js/main.js
@@ -6,3 +6,20 @@ $( window ).on('scroll', function(){
     }
     $('#navbar').css('padding', score)
 });
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        responsive: {
+            0: {
+                items:1
+            },
+            600: {
+                items:2
+            },
+            1000: {
+                items:3
+            }
+        }
+    });
+  });
